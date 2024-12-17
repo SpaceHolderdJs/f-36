79637d6c8c292c0ac338d6c9acbc221c9c0f6148
Igor <sergienko339@gmail.com>
2024-12-17 22:11:36 +0200

    Map, Set

--- JSBasics/Classes/static.js
+++ JSBasics/Classes/static.js
@@ -40,10 +40,6 @@ console.log(AdvancedCart.allShopGoods);
 console.log(AdvancedCart.discount);
 console.log(AdvancedCart.a);
 
-class Shop {}
-class ShopItem {}
-class ShopCart {}
-
 // Завдання:
 // описати класи слідуючи інструкції
 
@@ -65,3 +61,77 @@ class ShopCart {}
 // this
 
 // {title: "Pen", id: 1, price: 5}
+
+class Shop {
+  static catalog = [
+    { id: 1, price: 300, title: "Gloves" },
+    { id: 2, price: 700, title: "T-Shirt" },
+    { id: 3, price: 500, title: "Jacket" },
+  ];
+}
+
+class ShopItem {
+  constructor(id, price, title) {
+    this.id = id;
+    this.price = price;
+    this.title = title;
+  }
+
+  changePrice(newPrice) {
+    this.price = newPrice;
+  }
+
+  changeTitle(newTitle) {
+    this.title = newTitle;
+  }
+}
+
+class ShopCart {
+  static discount = 10;
+
+  constructor(items = []) {
+    this.items = items;
+  }
+
+  buy(item) {
+    this.items.push(item);
+  }
+
+  remove(item) {
+    this.items = this.items.filter(({ id }) => id !== item.id);
+  }
+
+  proceed(discount = ShopCart.discount) {
+    const totalPrice = this.items.reduce(
+      (total, { price }) => (total += price),
+      0
+    );
+
+    const totalPriceAfterDiscount = totalPrice - (totalPrice * discount) / 100;
+
+    console.log("Total checkout: ");
+    console.table(this.items);
+    console.log(`Total price: ${totalPrice} $`);
+    console.log(`Discount: ${discount} %`);
+    console.log(`Total: ${totalPriceAfterDiscount} $`);
+  }
+}
+
+const items = Shop.catalog.map(
+  ({ id, price, title }) => new ShopItem(id, price, title)
+);
+
+const homeworkCart = new ShopCart(items);
+
+homeworkCart.items.at(0).changePrice(100000);
+homeworkCart.items.at(1).changeTitle("New title");
+console.log(homeworkCart.items.at(0));
+console.log(homeworkCart.items.at(1));
+
+homeworkCart.buy(new ShopItem(4, 300, "Socks"));
+console.log(homeworkCart.items, "items after buy");
+
+homeworkCart.remove(new ShopItem(4, 300, "Socks"));
+console.log(homeworkCart.items, "items after remove");
+
+homeworkCart.proceed(100);
---
+++ JSBasics/DataStructures/main.js
@@ -0,0 +1,157 @@
+// []
+// {}
+
+// Map
+
+Map;
+
+const map = new Map([
+  ["a", 11],
+  ["b", 13],
+  [{}, {}],
+]);
+
+map.set("name", "Igor");
+map.set("items", [1, 2, 3, 4, 5, 6, 7]);
+map.set("name", "Oleg");
+
+const o1 = { a: 10 };
+const o2 = { a: 10 };
+
+map.set(o1, 1);
+map.set({ a: 10 }, 2);
+map.set({ a: 10 }, 3);
+map.set(o1, 4);
+
+map.set(
+  [].find((e) => e === 10),
+  "11-05-2021"
+);
+map.set(undefined, "11-07-2021");
+map.set(undefined, "11-07-2023");
+
+map.set("v", 0);
+
+map.set(null, "Hello");
+map.set(NaN, "NaN");
+
+console.log(map.get("null"));
+
+console.log(map.has(null));
+console.log(map.has(o1));
+
+console.log(map.has("key"));
+console.log(Boolean(map.get("key")));
+
+console.log(Boolean(map.get("v")));
+console.log(map.has("v"));
+console.log(map.has({ a: 10 }));
+
+// console.log(map.clear());
+
+console.log(map.delete("v"), "delete");
+console.log(map.delete(o1), "delete 2");
+
+console.log(map, "after delete");
+
+console.log([...map.keys()], "keys");
+console.log([...map.values()], "values");
+
+console.log(map.size, "size");
+
+map.forEach((value, key) => {
+  console.log(value, key, "forEach");
+
+  if (key !== null && typeof key === "object" && key.hasOwnProperty("a")) {
+    map.delete(key);
+  }
+});
+
+console.log(map);
+
+// const entries = Object.entries({ a: 10, b: 20 });
+// console.log(entries, "entries");
+
+// const obj = Object.fromEntries([
+//   ["a", 10],
+//   ["b", 12],
+// ]);
+
+// console.log(obj, "fromEntries");
+
+console.log(map);
+
+class Shop {
+  static visitors = new Map();
+}
+
+class Visitor {
+  constructor(name, budget, discount) {
+    this.name = name;
+    this.budget = budget;
+    this.discount = discount / 100;
+  }
+
+  visit() {
+    Shop.visitors.set(this, []);
+  }
+
+  leave() {
+    Shop.visitors.delete(this);
+  }
+
+  buy(shopItem) {
+    // Завдання
+    // Дописати метод (buy)
+    // який додасть до корзини конкретного Visitor (this)
+    // товар як еземпляр ShopItem
+
+    if (!Shop.visitors.has(this)) return;
+
+    if (Boolean(this.discount)) {
+      shopItem.discount(this.discount);
+    }
+
+    this.budget = this.budget - shopItem.price;
+
+    Shop.visitors.get(this).push(shopItem);
+  }
+}
+
+class ShopItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+
+  discount(d) {
+    this.price = this.price - this.price * d;
+  }
+}
+
+const visitor1 = new Visitor("Igor", 3000, 10);
+const visitor2 = new Visitor("Alina", 2500, 15);
+
+visitor1.visit();
+visitor2.visit();
+
+visitor1.buy(new ShopItem("Socks", 200));
+
+console.log(Shop.visitors, "visitor");
+
+// H/W
+
+// 1- Додати логіку видалення товару з корзини після оплати (buy)
+// 2- У методі leave (Visitor) додати логіку перевірки того, що корзитна пуста
+// 3- Додати перевірки budget (усюди де буде потрібно (на ваш розсуд))
+// 4- Додати то Visitor метод registerAdmin (корзина завджи пуста) (isAdmin: true)
+// Такий користувач не може купляти (поліморфізм, наслідування),
+// Додати метод до Admin closeShop (якщо нема відвідувачів) - закриває магазин
+
+// {a: 10}
+
+// {user: {}}
+
+// {{}:{}}
+
+// Set
